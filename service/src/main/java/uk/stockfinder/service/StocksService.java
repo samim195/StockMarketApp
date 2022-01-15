4e0b1f06158b18a60b3ac3255c64278392c58865
samim195 <samimkohistani@hotmail.com>
2022-01-15 16:57:05 +0000

    feat: Add stocks service

---
+++ service/src/main/java/uk/stockfinder/service/StocksService.java
@@ -0,0 +1,37 @@
+package uk.stockfinder.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.stockfinder.CsvReader;
+import uk.stockfinder.FileConstant;
+import uk.stockfinder.Stock;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class StocksService {
+
+    @Autowired
+    private CsvReader csvReader;
+
+    public StocksService(CsvReader csvReader) {
+        this.csvReader = csvReader;
+    }
+
+    public List<Stock> getAllStocks ()throws Exception {
+        csvReader = new CsvReader();
+        return csvReader.readStockFromFile(FileConstant.FILE_NAME);
+    }
+
+    public List<Stock> getStockByName(String stockName) throws Exception {
+        csvReader = new CsvReader();
+        List<Stock> stocks = csvReader.readStockFromFile(FileConstant.FILE_NAME);
+        List<Stock> collection = stocks.stream().filter(stock -> stock.getName().equals(stockName)).collect(Collectors.toList());
+        return collection;
+    }
+
+    public Stock getStockByName() {
+        return null;
+    }
+}
