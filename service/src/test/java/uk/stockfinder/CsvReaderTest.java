afc0ef3069aa9e28d7f45d32e3aeb2191ab8ff09
samimkohistani <samimkohistani@hotmail.com>
2022-01-19 23:05:48 +0000

    refactor: give more meaningful name

--- service/src/test/java/uk/stockfinder/CsvReaderTest.java
+++ service/src/test/java/uk/stockfinder/CsvReaderTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class CsvReaderTest {
 
     @Test
-    void test() throws Exception {
+    void givenStockListVerifyStockObjectContainsSpecificData() throws Exception {
         //When
         CsvReader csvReader = new CsvReader();
         Stock build = Stock.builder()
